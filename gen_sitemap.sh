f2ebc3e8d321472fdacb4433da7772dd6cc5cb06
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-05-31 10:47:30 +0800

    fix sitemap generation

--- gen_sitemap.sh
+++ gen_sitemap.sh
@@ -1,1 +1,4 @@
-mdbook-sitemap-generator -d sichanghe.github.io -o _site/sitemap.xml
+# Patch modified from <https://github.com/rxdn/mdbook-sitemap-generator/issues/2#issuecomment-1936899785>.
+mdbook-sitemap-generator -d sichanghe.github.io \
+    | awk '{gsub(/urls>/, "url>"); gsub(/xlmns/, "xmlns"); print}' \
+        > _site/sitemap.xml
