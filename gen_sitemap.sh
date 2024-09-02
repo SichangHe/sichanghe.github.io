30ed2ece0836208c2147e075e64af5535a6f6b9c
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-09-01 20:25:56 -0700

    rid mdbook-sitemap-generator and use generic sitemap generator

--- gen_sitemap.sh
+++
@@ -1,4 +0,0 @@
-# Patch modified from <https://github.com/rxdn/mdbook-sitemap-generator/issues/2#issuecomment-1936899785>.
-mdbook-sitemap-generator -d sichanghe.github.io \
-    | awk '{gsub(/urls>/, "url>"); gsub(/xlmns/, "xmlns"); print}' \
-        > _site/sitemap.xml
